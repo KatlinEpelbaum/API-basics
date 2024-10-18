25f527d70f88ccc618de575c2ec237e6dd42ee02
Kätlin <145542776+KatlinEpelbaum@users.noreply.github.com>
2024-10-18 11:41:26 +0300

    update

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import carRoutes from "./routes/carRoutes.js";
+
+dotenv.config();
+
+const PORT = import.meta.PORT || 3006;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.json({
+        message: "hello from server",
+    });
+});
+
+app.use(carRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
