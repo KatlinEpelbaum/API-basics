fde0c9613b33fe28cb3e272eb161a83ef1110fba
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-10-30 11:02:38 +0200

    update

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,6 +1,64 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { response } from 'express';
 
 const prisma = new PrismaClient()
+
+export const deleteBook = async (request, response) => {
+    try {
+        const { id } = request.params; 
+         await prisma.book.delete({
+            where: { id }
+        });
+
+        response.status(200).json({
+            message: "The book is gone"
+        });
+        
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            message: "It aint deleted"
+        });
+    }
+};
+
+export const getAllBooks = async (request, response) => {
+    try {
+        const books = await prisma.book.findMany();
+
+        response.status(200).json({
+            books
+        });
+        
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            message: "Something went wrong, womp womp"
+        });
+    }
+};
+export const getBook = async (request, response) => {
+    try {
+        const { id } = request.params
+        const book = await prisma.book.findUnique({
+            where: { id }
+        })
+
+        if(book) {
+            return response.status(404).json({
+                message: "Book aint here girl"
+            })
+        }
+        response.status(200).json({
+            book
+        })
+    } catch (error) {
+        console.log(error);
+        response.status(500).json({
+            message: "Something went wrong, womp womp"
+        });
+    }
+};
 
 export const createBook = async (request, response) => {
     const { title, description, year, author, publisher } = request.body;
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { createBook, updateBook } from '../controllers/book.controller.js'
+import { createBook, updateBook, getAllBooks, getBook, deleteBook } from '../controllers/book.controller.js'
 import { validate } from '../middlewares/validate.middleware.js'
 import { bookSchema } from '../validation/book.validate.js'
 
 const router = Router();
 
+router.get('/books', getAllBooks)
+router.get('book/:id', getBook)
 router.post('/books',validate(bookSchema), createBook);
 router.put('/books/:id',validate(bookSchema), updateBook);
+router.delete('/books/:id', deleteBook);
 
 export default router;  
